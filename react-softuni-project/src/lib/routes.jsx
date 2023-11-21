ae3e2d717215f02a6f177358cd283cc47aa6efa1
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-11-22 00:35:09 +0200

    added users and profile routes

--- react-softuni-project/src/lib/routes.jsx
+++ react-softuni-project/src/lib/routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Login from "../components/auth/Login";
-import Register from "../components/auth/Register"
+import Register from "../components/auth/Register";
 import Layout from "../components/layout";
+import Dashboard from "../components/dashboard/index";
+
 
 export const ROOT = "/";
 export const LOGIN = "/login";
@@ -10,16 +12,29 @@ export const REGISTER = "/register";
 
 export const PROTECTED = "/protected";
 export const DASHBOARD = "/protected/dashboard";
+export const USERS = "/protected/users";
+export const PROFILE = "/protected/profile/:id";
 
 export const router = createBrowserRouter([
     { path: ROOT, element: "Public Root" },
-    { path: LOGIN, element: <Login/> },
+    { path: LOGIN, element: <Login /> },
     { path: REGISTER, element: <Register /> },
-    { 
-        path: PROTECTED, 
-        element: <Layout />, 
-        children:[{
-            path: DASHBOARD, 
-            element: "Dashboard"
-    },], },
+    {
+        path: PROTECTED,
+        element: <Layout />,
+        children: [
+            {
+                path: DASHBOARD,
+                element: <Dashboard />,
+            },
+            {
+                path: USERS,
+                element: "Users",
+            },
+            {
+                path: PROFILE,
+                element: "User Profile for specific ID",
+            },
+        ],
+    },
 ]);
