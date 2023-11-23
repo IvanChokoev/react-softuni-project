6fc7a05f67e80f15ec14c30f090dfc3e168d9ede
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-11-23 02:14:37 +0200

    added post firld in dashboard

--- react-softuni-project/src/components/dashboard/index.jsx
+++ react-softuni-project/src/components/dashboard/index.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useAuth } from "../../hooks/auth";
+import { usePost } from '../../hooks/posts';
+import { useState } from 'react';
+import "../dashboard/index.css"
+import { useAddPost } from '../../hooks/posts';
+import PostsLists from '../post/PostsLists';
+
+
+function NewPost(){
+  const { register, handleSubmit, reset} = useForm();
+  const {addPost, isLoading: addingPost} = useAddPost();
+  const {user, isLoading: authLoading} = useAuth();
+  const [loading, setLoading] = useState(false);
+  
+  async function handleAddPost(data) {
+    try {
+      setLoading(true); // Set loading to true before the async operation
+      await addPost({
+        uid: user.id,
+        text: data.text,
+      });
+      reset();
+    } finally {
+      setLoading(false); // Set loading to false after the async operation completes
+    }
+  }
+
+  return (
+    <div className="new-post">
+      <form onSubmit={handleSubmit(handleAddPost)}>
+        <div className="form-div">
+          <h2 className='h2'>New Post</h2>
+          <button
+            className="custom-button"
+            type="submit"
+            disabled={loading || authLoading || addingPost}
+            >
+            {loading ? 'Loading' : 'Post'}
+          </button>
+        </div>
+        <textarea
+          name="post-area"
+          id="post-area"
+          cols="30"
+          rows="3"
+          placeholder='Create a new post...'
+          {...register("text", { required: true })}>
+        </textarea>
+      </form>
+    </div>
+  )
+}
+
 
 export default function Dashboard() {
+  const { posts, isLoading } = usePost();
+
+  if(isLoading) return "Loading posts...";
+
   return (
-    <div>Dashboard</div>
+    <>
+      <NewPost />
+      <PostsLists posts={posts}/>
+    </>
   )
 }
