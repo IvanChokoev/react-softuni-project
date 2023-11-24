7a9adc7a64da2971db947fea2c1ce00d640cf7b5
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-11-24 14:03:08 +0200

    moved date and time formating to be reusable

--- react-softuni-project/src/components/post/Header.jsx
+++ react-softuni-project/src/components/post/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Avatar} from "@chakra-ui/react";
+import UserAvatar from "../../components/profile/Avatar.jsx";
 import { useUser } from '../../hooks/users';
 import { PROTECTED } from "../../lib/routes";
 import { Link } from "react-router-dom";
+import  CalculateRelativeTime  from "../../utils/dateAndTime.jsx"
+import UsernameButton from '../profile/UsernameButton.jsx';
 
 export default function Header({ post }) {
     const {uid, date} = post;
@@ -10,37 +12,15 @@ export default function Header({ post }) {
 
     if (isLoading) return "Loading...";
 
-    const calculateRelativeTime = (timestamp) => {
-        const now = new Date();
-        const createdAt = new Date(timestamp);
-
-        const timeDifference = now - createdAt;
-        const seconds = Math.floor(timeDifference / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-
-        if (days > 0) {
-            return `${days} day${days !== 1 ? 's' : ''} ago`;
-        } else if (hours > 0) {
-            return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-        } else if (minutes > 0) {
-            return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
-        } else {
-            return `less than a minute ago`;
-        }
-    };
-
     return (
+        ///try to fix the issue with Avatar. FIXED
         <div className='header'>
-            <Link to={`${PROTECTED}/profile/${user?.id}`} className="avatar-container">
-                <Avatar name={user.username} />
-            </Link>
+            <div className='avatar-container'>
+            <UserAvatar user={user} size='md' />
+            </div>
             <div className="user-info-container">
-                <Link to={`${PROTECTED}/profile/${user?.id}`}>
-                    <button className="button-style">@{user.username}</button>
-                </Link>
-                <span className="text-style">{calculateRelativeTime(date)} </span>
+                <UsernameButton user={user}/>
+                <span className="text-style">{CalculateRelativeTime(date)} </span>
             </div>
         </div>
 
