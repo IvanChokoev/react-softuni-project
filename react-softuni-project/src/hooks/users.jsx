b39bcca6d784d4aa6f99988e45322b65df4757a3
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-12-01 16:48:43 +0200

    added querry from firebase to users

--- react-softuni-project/src/hooks/users.jsx
+++ react-softuni-project/src/hooks/users.jsx
@@ -1,9 +1,9 @@
 import {useDocumentData} from "react-firebase-hooks/firestore";
 import { db } from "../lib/firebase";
-import { doc, query } from "firebase/firestore"; 
+import { doc, query} from "firebase/firestore";
 
 export function useUser(id) {
-    const q = query(doc(db, "users", id));
+    const q = query(doc(db, "users", id));    
     const [user, isLoading] = useDocumentData(q);
     
     return { user, isLoading };
