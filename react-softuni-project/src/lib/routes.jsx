5b11637bc599a61975ad1ba0ddc2932aa3e4fd7c
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-12-02 16:58:51 +0200

    added User route

--- react-softuni-project/src/lib/routes.jsx
+++ react-softuni-project/src/lib/routes.jsx
@@ -6,6 +6,7 @@ import Layout from "../components/layout";
 import Dashboard from "../components/dashboard/index";
 import Comments from "../components/comments";
 import Profile from "../components/profile";
+import Users from "../components/users";
 
 
 export const ROOT = "/";
@@ -19,9 +20,9 @@ export const PROFILE = "/protected/profile/:id";
 export const COMMENTS = "/protected/comments/:id";
 
 export const router = createBrowserRouter([
+    { path: ROOT, element: <Login /> },
     { path: LOGIN, element: <Login /> },
     { path: REGISTER, element: <Register /> },
-    { path: ROOT, element: <Login /> },
     {
         path: PROTECTED,
         element: <Layout />,
@@ -32,7 +33,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: USERS,
-                element: "Users",
+                element: <Users/>,
             },
             {
                 path: PROFILE,
