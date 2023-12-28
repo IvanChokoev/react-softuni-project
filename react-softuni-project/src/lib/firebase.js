5954b3d5b735ce5f1105e6291c0b05011971b81c
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-12-28 17:37:31 +0200

    added env to firebase

--- react-softuni-project/src/lib/firebase.js
+++ react-softuni-project/src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCDg0OwP-6c5HbibfMRpwT3czFJ7TZ5wzg",
+    authDomain: "react-softuni-project-949c9.firebaseapp.com",
+    projectId: "react-softuni-project-949c9",
+    storageBucket: "react-softuni-project-949c9.appspot.com",
+    messagingSenderId: "282818115734",
+    appId: "1:282818115734:web:905b12b4ac881d48166ab7"
 };
 
 // Initialize Firebase
